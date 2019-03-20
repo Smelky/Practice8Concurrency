f0ab4a7b62671bac73a8003f01ad7d17d5115214
Smelky <46086711+Smelky@users.noreply.github.com>
2019-03-20 11:43:28 +0200

    Changed

--- src/com/concurrencyframefork/callable/CallableExample.java
+++ src/com/concurrencyframefork/callable/CallableExample.java
@@ -3,11 +3,11 @@ package com.concurrencyframefork.callable;
 import java.util.Random;
 import java.util.concurrent.Callable;
 
-public class CallableExample implements Callable {
+public class CallableExample implements Callable<Integer> {
 
-    public Object call() throws Exception {
+    public Integer call() throws Exception {
         Random generator = new Random();
-        Integer randomNumber = generator.nextInt(5);
+        int randomNumber = generator.nextInt(5);
 
         Thread.sleep(randomNumber * 1000);
 
